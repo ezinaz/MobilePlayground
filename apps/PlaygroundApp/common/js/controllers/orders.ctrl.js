cf48e669b31ac7393ebb8eef2a4bc919a8c90591
Kelley <kelley.smith@avnet.com>
2016-04-27 10:50:37 -0700

    List items styling

--- apps/PlaygroundApp/common/js/controllers/orders.ctrl.js
+++ apps/PlaygroundApp/common/js/controllers/orders.ctrl.js
@@ -31,6 +31,17 @@ function OrdersCtrl($scope, Orders, moment) {
         console.log('getOrders: ', $scope.searchObject);
 
         Orders.fetchOrders($scope.searchObject).then(function (orders) {
+        	for (i = 0; i < orders.length; i++) { 
+        	    if (orders[i].StatusCode == 'A') {
+        	    	orders[i].StatusCode = 'Open';
+        	    }
+        	    else if (orders[i].StatusCode == 'B') {
+        	    	orders[i].StatusCode = 'Partial';
+        	    }
+        	    else if (orders[i].StatusCode == 'C') {
+        	    	orders[i].StatusCode = 'Closed';
+        	    }
+        	}
             $scope.orders = orders;
         });
 
